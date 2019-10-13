d3faa9d6cefbcd25e4554507cdf95bbaa4190a15
Duncan Riach <duncan@riach.org>
2019-10-13 19:17:22 +0000

    Fixed the operation of moving to target for all cases

--- index.js
+++ index.js
@@ -51,15 +51,11 @@ window.onload = () => {
 			this.focused = false;
 		}
 
-		pointAt() {
-			
-		}
-
 		move(step) {
 			this.x += step * MyMath.cos(this.d);
 			this.y += step * MyMath.sin(this.d);
 		}
-		
+
 		can_see(target) {
 			if (Math.hypot(this.x - target.x, this.y - target.y) <= (this.vision + this.r + target.r)) {
 				return true;
@@ -70,42 +66,56 @@ window.onload = () => {
 
 		// Ideally, this would be a private method
 		angle_to(target) {
-			let dx = target.x - this.x;
-			let dy = target.y - this.y;
+			/*
+			Remember that x increases left-to-right, but y increases
+			top-to-bottom. So angle increases clockwise.
+			*/
+			let dx = target.x - this.x; // x-component of vector pointing at target
+			let dy = target.y - this.y; // y-component of vector pointing at target
 			let angle = MyMath.atan(dy/dx);
 
 			if (dx == 0) {
 				// Handle case where dy/dx is undefined.
 				if (dy > 0) {
-					// Below target
+					// Target is below
 					return 90;
 				} else {
-					// Above target
+					// Target is above
 					return 270;
 				}
 			} else if (dy > 0) {
 				if (dx > 0) {
-					// Top-right quadrant
+					// Bottom-right quadrant
 					angle = angle;
 				} else {
-					// Top-left quadrant
-					angle = 90 - angle;
+					// Bottom-left quadrant
+					angle = 180 + angle;
 				}
 			} else {
 				if (dx > 0) {
-					// Bottom-right quadrant
+					// Top-right quadrant
 					angle = 360 + angle;
 				} else {
-					// Bottom-left quadrant
+					// top-left quadrant
 					angle = 180 + angle;
 				}
 			}
 			return angle;
 		}
 
+		reached(target) {
+			let threshold = this.s * 1.5;
+			if (Math.hypot(this.x - target.x, this.y - target.y) <= threshold) {
+				return true;
+			}
+			return false;
+		}
+
 		update() {
 
-			this.move(this.s);
+			if (!this.reached(blob)) {
+				this.move(this.s);
+			}
 
 			if (this.can_see(blob)) {
 				this.d = this.angle_to(blob);
@@ -142,14 +152,24 @@ window.onload = () => {
 
 
 	// let dot = new Agent(450, 22, 20, Math.random() * 90, [0, 100, 100], 2);
-	let dot = new Agent(450, 22, 20, 250, [0, 100, 100], 2);
+	let dot1 = new Agent(50, 50, 20, 40, [0, 100, 100], 2);
+	let dot2 = new Agent(450, 50, 20, 120, [100, 0, 100], 2);
+	let dot3 = new Agent(450, 450, 20, 35, [100, 100, 0], 2);
+	let dot4 = new Agent(50, 450, 20, 360-20, [0, 0, 0], 2);
+	let dot5 = new Agent(250, 50, 20, 90, [50, 100, 150], 2);
+	let dot6 = new Agent(250, 450, 20, 270, [255, 100, 150], 2);
 	let blob = new Food(250, 250);
 
 	function draw() {
 		ctx.fillStyle = 'white';
 		ctx.fillRect(0, 0, WIDTH, HEIGHT);
 		blob.render();
-		dot.render();
+		dot1.render();
+		dot2.render();
+		dot3.render();
+		dot4.render();
+		dot5.render();
+		dot6.render();
 		window.requestAnimationFrame(draw);
 	}
 	draw();
