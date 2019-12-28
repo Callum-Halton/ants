6a631d62d1f7ff2940d6d01b9223fa399456e2b2
Duncan Riach <duncan@riach.org>
2019-12-28 21:14:18 +0000

    Moved CogHelper from terrain.js into cogHelper.js

--- src/simulation/terrain.js
+++ src/simulation/terrain.js
@@ -1,37 +1,10 @@
 import { MyMath, colorString, Point } from './utils.js';
 import Colony from './colony.js';
 import { Cell, CellWithLocation, CellLoc } from './cell.js';
+import CogHelper from './cogHelper.js';
 
 const GRID_SIZE = 20;
 const MAX_AGENTS = 200;
-
-class CogHelper {
-  constructor(defaultPosition, markerType) {
-    this.markerType = markerType; // this is a string.
-    this._defaultPosition = defaultPosition;
-    this._total = 0;
-    this._nonNormalizedCog = new Point(0, 0);
-  }
-  update(/* CellWithLocation */ cellWithLocation) {
-   let markerValue = cellWithLocation.cell[this.markerType];
-   this._nonNormalizedCog.x += cellWithLocation.loc.x * markerValue;
-   this._nonNormalizedCog.y += cellWithLocation.loc.y * markerValue;
-   this._total += markerValue;
-  }
-  getNormalizedCog() {
-    if (this._total > 0) {
-      let normalizedCog = new Point(this._nonNormalizedCog.x / this._total,
-                                    this._nonNormalizedCog.y / this._total);
-      // ctx.fillStyle = "#FF0000";
-      // ctx.beginPath();
-      // ctx.arc(normalizedCog.x, normalizedCog.y, 5, 0, 2 * Math.PI);
-      // ctx.fill();
-      return normalizedCog;
-    } else {
-      return this._defaultPosition;
-    }
-  }
-}
 
 export default class Terrain {
   // Constructs a terrain of size width and height, consisting of square
